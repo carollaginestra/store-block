f21d2359d5edb5fab9e255f22612ac54f36a40d2
carollaginestra <carolina.laginestra@driven.cx>
2020-08-12 14:37:14 -0300

    Curso VTEX IO - store block

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TimeSplit } from './typings/global'
+import { tick } from './utils/time'
+import { useCssHandles } from 'vtex.css-handles'
+import { useQuery } from 'react-apollo'
+import useProduct from 'vtex.product-context/useProduct'
+import productReleaseDate from './queries/productReleaseDate.graphql'
 
-interface CountdownProps { }
-
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
+interface CountdownProps {
+  targetDate: string
 }
+
+const CSS_HANDLES = ["countdown"]
+
+const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString()
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({}) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  })
+
+  const handles = useCssHandles(CSS_HANDLES)
+
+  const { product: { linkText } } = useProduct()
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+      slug: linkText
+    },
+    ssr: false
+    
+  })
+
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Erro!</span>
+      </div>
+    )
+  }
+
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
+
+  return (
+    <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+    </div>
+  )
+  
+}
+
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+      title: 'editor.countdown.targetDate.title',
+      description: 'editor.countdown.targetDate.description',
+      type: 'string',
+      default: null,
+    },
+  },
 }
 
 export default Countdown
